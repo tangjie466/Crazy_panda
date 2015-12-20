bc201f5413c1307ed9c0b46beaba24c4cc286529
唐杰 <tangjie466@icloud.com>
2015-12-20 20:47:15 +0800

    广告后不能进入场景的bug

--- Assets/_script/load/load_scence.cs
+++ Assets/_script/load/load_scence.cs
@@ -5,6 +5,7 @@ public class load_scence : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		Time.timeScale = 1;
         Debug.Log("load_1");
         StartCoroutine(load_next());
 	}
@@ -15,8 +16,9 @@ public class load_scence : MonoBehaviour {
 	}
 
     IEnumerator load_next() {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.5f);
         Debug.Log("load_1");
+		Debug.Log("gate name is "+gate_common.next_gate);
        Application.LoadLevelAsync(gate_common.next_gate);
         Debug.Log("load_2");
         StopCoroutine(load_next());
